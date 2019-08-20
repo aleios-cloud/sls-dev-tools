dd7e47827913cb1b9828c284b5454db765790749
Ben Ellerby <bene@theodo.co.uk>
2019-08-20 20:00:32 +0100

    ink working

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import {render, Box} from 'ink';
+import {render, Box, Static, TestResults} from 'ink';
+
+const funcs = [
+	{ title: "function 1", id: 1},
+	{ title: "function 2", id: 2},
+	{ title: "function 3", id: 3},
+]
 
 const Demo = () => (
-	<Box>
-		Hello World
-	</Box>
+<>
+	<Static>
+		{funcs.map(func => (
+			func.title
+		))}
+	</Static>
+</>
 );
 
 render(<Demo/>);

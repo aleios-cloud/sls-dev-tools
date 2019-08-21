6d8e36160bd124da4d96ba55e3943895aa4383b1
robcronin <robc@theodo.co.uk>
2019-08-21 19:46:21 +0100

    Format index file

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
 import AWS from 'aws-sdk';
-import {render, Color, Box} from 'ink';
+import { render, Color, Box } from 'ink';
 import Spinner from 'ink-spinner';
 
-const stackName = "example stack"
-
+const stackName = 'example stack';
 
 const logo = `
 ___                      _             ___            _____         _    
@@ -13,76 +12,85 @@ ___                      _             ___            _____         _
 |___/\\___|_|  \\_/\\___|_| |_\\___/__/__/ |___/\\___|\\_/    |_|\\___/\\___/_/__/
 																		   
 
-`
+`;
 
 class Demo extends Component {
-	constructor(props) {
-		super(props);
-		this.state = { funcs: [], logGroups: [] };
-	}
+  constructor(props) {
+    super(props);
+    this.state = { funcs: [], logGroups: [] };
+  }
 
-	componentDidMount() {
-		this.cloudformation = new AWS.CloudFormation({ region: 'eu-west-2'});
-		this.cloudwatchLogs = new AWS.CloudWatchLogs({ region: 'eu-west-2'});
-		this.getLambdasForStackName(stackName);
-		this.getLogs(stackName)
-	}
+  componentDidMount() {
+    this.cloudformation = new AWS.CloudFormation({ region: 'eu-west-2' });
+    this.cloudwatchLogs = new AWS.CloudWatchLogs({ region: 'eu-west-2' });
+    this.getLambdasForStackName(stackName);
+    this.getLogs(stackName);
+  }
 
-	getLambdasForStackName(stackName) {
-		const that = this;
-		return this.cloudformation.listStackResources({ StackName : stackName }, function(err, data) {
-			if (err) {
-				console.log(err, err.stack);
-			}
-			else {
-				that.setState({
-					funcs: data.StackResourceSummaries.filter(res => res.ResourceType === "AWS::Lambda::Function"),
-				})
-			}
-		});
-	}
+  getLambdasForStackName(stackName) {
+    const that = this;
+    return this.cloudformation.listStackResources(
+      { StackName: stackName },
+      function(err, data) {
+        if (err) {
+          console.log(err, err.stack);
+        } else {
+          that.setState({
+            funcs: data.StackResourceSummaries.filter(
+              res => res.ResourceType === 'AWS::Lambda::Function',
+            ),
+          });
+        }
+      },
+    );
+  }
 
-	getLogs(stackName) {
-		const that = this;
-		var params = {
-			logGroupNamePrefix: `/aws/lambda/${stackName}`,
-		};
-		  that.cloudwatchLogs.describeLogGroups(params, function(err, data) {
-			if (err) {
-				console.log(err, err.stack);
-			}
-			else {
-				that.setState({logGroups: data.logGroups})
-			}
-		  });
-	}
+  getLogs(stackName) {
+    const that = this;
+    var params = {
+      logGroupNamePrefix: `/aws/lambda/${stackName}`,
+    };
+    that.cloudwatchLogs.describeLogGroups(params, function(err, data) {
+      if (err) {
+        console.log(err, err.stack);
+      } else {
+        that.setState({ logGroups: data.logGroups });
+      }
+    });
+  }
 
-	render() {
-		return (
-			<>
-				<Color green>
-					{logo}
-				</Color>
-				--- Functions ---
-					{this.state.funcs.map(func => (
-						<div style={{display: "flex", flexDirection: "row"}} key={func.PhysicalResourceId}>
-							<div>{"\u03BB "}</div>
-							<div>{func.LogicalResourceId}</div>
-						</div>
-					))}
-				{""}
-				--- Log Group ----
-					{this.state.logGroups.map(logGroup => (
-						<div style={{display: "flex", flexDirection: "row"}} key={logGroup.arn}>
-								<>
-									<Color green><Spinner type="dots"/></Color>
-								</>
-							<div>{`  ${logGroup.logGroupName}`}</div>
-						</div>
-					))}
-			</>
-		)
-	}
-};
+  render() {
+    return (
+      <>
+        <Color green>{logo}</Color>
+        --- Functions ---
+        {this.state.funcs.map(func => (
+          <div
+            style={{ display: 'flex', flexDirection: 'row' }}
+            key={func.PhysicalResourceId}
+          >
+            <div>{'\u03BB '}</div>
+            <div>{func.LogicalResourceId}</div>
+          </div>
+        ))}
+        {''}
+        --- Log Group ----
+        {this.state.logGroups.map(logGroup => (
+          <div
+            style={{ display: 'flex', flexDirection: 'row' }}
+            key={logGroup.arn}
+          >
+            <>
+              <Color green>
+                <Spinner type="dots" />
+              </Color>
+            </>
+            <div>{`  ${logGroup.logGroupName}`}</div>
+          </div>
+        ))}
+      </>
+    );
+  }
+}
 
-render(<Demo/>);
+render(<Demo />);

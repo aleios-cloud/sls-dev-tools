4f27a7496973350910df9f44eeb02e07175e07aa
Joseph Mckenzie <joseph.mckenzie@poetic.io>
2019-11-20 11:51:30 -0500

    put back missing flag

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 /* eslint-disable no-plusplus */
 /* eslint-disable no-console */
 /* eslint-disable new-cap */
-
 import AWS from 'aws-sdk';
 import { awsRegionLocations, logo, dateFormats } from './constants';
 
@@ -17,15 +16,13 @@ program
   .option('-r, --region <region>', 'AWS region')
   .option('-t, --start-time <startTime>', 'when to start from')
   .option('-i, --interval <interval>', 'interval of graphs, in seconds')
-  .option('-p, --profile <profile>', 'aws profile name to use')
+  .option('-p, --period <period>', 'precision of graphs, in seconds')
   .parse(process.argv);
 
 const screen = blessed.screen({ smartCSR: true });
-const profile = program.profile || 'default'
-const credentials = new AWS.SharedIniFileCredentials({ profile: profile });
-const cloudformation = new AWS.CloudFormation({ region: program.region, credentials: credentials });
-const cloudwatch = new AWS.CloudWatch({ region: program.region, credentials: credentials });
-const cloudwatchLogs = new AWS.CloudWatchLogs({ region: program.region, credentials: credentials });
+const cloudformation = new AWS.CloudFormation({ region: program.region });
+const cloudwatch = new AWS.CloudWatch({ region: program.region });
+const cloudwatchLogs = new AWS.CloudWatchLogs({ region: program.region });
 
 function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
@@ -102,7 +99,7 @@ class Main {
 
     this.funcName = null;
 
-    this.interval = program.interval || 3600; // 1 hour
+    this.period = program.period || 3600; // 1 hour
     this.endTime = new Date();
     if (program.startTime) {
       // eslint-disable-next-line prefer-destructuring
@@ -110,9 +107,9 @@ class Main {
     } else {
       const dateOffset = (24 * 60 * 60 * 1000); // 1 day
 
-      // Round to closest interval to make query faster.
+      // Round to closest period to make query faster.
       this.startTime = new Date(
-        (Math.round(new Date().getTime() / this.interval) * this.interval) - dateOffset,
+        (Math.round(new Date().getTime() / this.period) * this.period) - dateOffset,
       );
     }
   }
@@ -182,7 +179,7 @@ class Main {
       for (
         let timestamp = moment(this.startTime).valueOf();
         timestamp < moment(this.endTime).valueOf();
-        timestamp = moment(timestamp).add(this.interval, 'seconds').valueOf()
+        timestamp = moment(timestamp).add(this.period, 'seconds').valueOf()
       ) {
         if (this.data.MetricDataResults[index].Timestamps.every(
           (it) => it.valueOf() !== timestamp,
@@ -219,7 +216,7 @@ class Main {
       style: { line: 'green' },
       x: this.data.MetricDataResults[2].Timestamps.map((d) => {
         const start = moment(d).format(dateFormat);
-        const end = moment(d).add(this.interval, 'seconds').format(dateFormat);
+        const end = moment(d).add(this.period, 'seconds').format(dateFormat);
         return `${start}-${end}`;
       }),
       y: this.data.MetricDataResults[2].Values,
@@ -318,7 +315,7 @@ class Main {
               MetricName: 'Duration',
               Namespace: 'AWS/Lambda',
             },
-            Period: this.interval,
+            Period: this.period,
             Stat: 'Maximum',
           },
           ReturnData: true,
@@ -340,7 +337,7 @@ class Main {
               MetricName: 'Errors',
               Namespace: 'AWS/Lambda',
             },
-            Period: this.interval,
+            Period: this.period,
             Stat: 'Sum',
           },
           ReturnData: true,
@@ -362,7 +359,7 @@ class Main {
               MetricName: 'Invocations',
               Namespace: 'AWS/Lambda',
             },
-            Period: this.interval,
+            Period: this.period,
             Stat: 'Sum',
           },
           ReturnData: true,

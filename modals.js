04995f7be5724eea7d7e44e7ccb0f119ba3f16ad
mansurpasha <mansur.a.pasha@gmail.com>
2020-04-01 11:49:40 +0100

    Created eventInject modal

--- modals.js
+++ modals.js
@@ -63,6 +63,33 @@ const helpModal = (screen, blessed) => {
   });
 };
 
+const eventInjectionModal = (screen, blessed, eventBridge) => {
+  const eventInjectLayout = blessed.layout({
+    parent: screen,
+    top: 'center',
+    left: 'center',
+    width: 112,
+    height: 27,
+    border: 'line',
+    style: { border: { fg: 'green' } },
+  });
+  blessed.box({
+    parent: eventInjectLayout,
+    width: 110,
+    height: 4,
+    left: 'right',
+    top: 'center',
+    align: 'center',
+    padding: { left: 2, right: 2 },
+    border: 'line',
+    style: { fg: 'green', border: { fg: 'green' } },
+    content: 'ESC to close',
+  });
+  screen.key(['escape'], () => {
+    eventInjectLayout.destroy();
+  });
+};
 module.exports = {
   helpModal,
+  eventInjectionModal,
 };

4ba1de779678dfbda2a5aa0cc05e2d1f27ba9874
Antoine Eudes <antoine.eudes@gmail.com>
2020-04-21 16:08:00 +0200

    :recycle: refacto boxes

--- src/modals/lambdaStatisticsModal.js
+++ src/modals/lambdaStatisticsModal.js
@@ -1,51 +1,13 @@
 import { DurationBarChart } from '../components/durationBarChart';
 import { ErrorDonutChart } from '../components/errorDonutChart';
+import { Box } from '../components/box';
+import { ModalLayout } from '../components/modalLayout';
 
-const lambdaStatisticsModal = (screen, blessed, application, lambdaName, cloudwatchLogs, cloudwatch) => {
-    const lambdaStatisticsLayout = blessed.layout({
-        parent: screen,
-        top: 'center',
-        left: 'center',
-        width: 112,
-        height: 27,
-        border: 'line',
-        style: { border: { fg: 'green' } },
-        keys: true,
-    });
-    blessed.box({
-        parent: lambdaStatisticsLayout,
-        width: 110,
-        height: 3,
-        left: "right",
-        top: "center",
-        align: "center",
-        padding: { left: 2, right: 2 },
-        border: "line",
-        style: { fg: "green", border: { fg: "green" } },
-        content: `Lambda Statistics - ${lambdaName}`,
-    });
-    const durationChartBox = blessed.box({
-        parent: lambdaStatisticsLayout,
-        width: 55,
-        height: 22,
-        left: 'right',
-        top: 'center',
-        align: 'center',
-        padding: { left: 2, right: 2 },
-        border: 'line',
-        style: { fg: 'green', border: { fg: 'green' } },
-    });
-    const errorChartBox = blessed.box({
-        parent: lambdaStatisticsLayout,
-        width: 55,
-        height: 22,
-        left: 'right',
-        top: 'center',
-        align: 'center',
-        padding: { right: 2, left: 2 },
-        border: 'line',
-        style: { fg: 'green', border: { fg: 'green' } },
-    });
+const lambdaStatisticsModal = (screen, application, lambdaName, cloudwatchLogs, cloudwatch) => {
+    const lambdaStatisticsLayout = new ModalLayout(screen, 112, 27, false);
+    new Box(lambdaStatisticsLayout, 110, 3, `Lambda Statistics - ${lambdaName}`);
+    const durationChartBox = new Box(lambdaStatisticsLayout, 55, 22);
+    const errorChartBox = new Box(lambdaStatisticsLayout, 55, 22);
     const durationChart = new DurationBarChart(application, cloudwatchLogs, false, durationChartBox)
     const errorChart = new ErrorDonutChart(application, errorChartBox, cloudwatch);
     durationChart.updateData(lambdaName);

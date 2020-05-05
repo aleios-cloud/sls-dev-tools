cb0174b52eb50b6da3d2560eabb232a5e1d8d66e
Gavin Williams <gavinw@theodo.co.uk>
2020-05-05 09:31:50 +0100

    Deploy relay API

--- src/services/apiGateway.js
+++ src/services/apiGateway.js
@@ -9,11 +9,11 @@ class ApiGateway {
       ProtocolType: "WEBSOCKET",
       RouteSelectionExpression: "$request.body.action",
     };
-    this.apiGateway.createApi(params, (err, data) => {
-      if (err) console.error(err, err.stack);
+    this.apiGateway.createApi(params, (createError, createData) => {
+      if (createError) console.error(createError, createError.stack);
       else {
         const integrationParams = {
-          ApiId: data.ApiId,
+          ApiId: createData.ApiId,
           IntegrationType: "AWS_PROXY",
           IntegrationMethod: "POST",
           IntegrationUri: `arn:aws:apigateway:${program.region}:lambda:path/2015-03-31/functions/${fullLambda.FunctionArn}/invocations`,
@@ -21,8 +21,41 @@ class ApiGateway {
         this.apiGateway.createIntegration(
           integrationParams,
           (integrationError, integrationData) => {
-            if (err) console.error(integrationError, integrationError.stack);
-            else console.log(integrationData);
+            if (integrationError)
+              console.error(integrationError, integrationError.stack);
+            else {
+              const routeParams = {
+                ApiId: createData.ApiId,
+                RouteKey: "$connect",
+                Target: `integrations/${integrationData.IntegrationId}`,
+              };
+              this.apiGateway.createRoute(routeParams, (routeError) => {
+                if (routeError) console.error(routeError, routeError.stack);
+                else {
+                  const stageParams = {
+                    ApiId: createData.ApiId,
+                    StageName: "relay-dev",
+                  };
+                  this.apiGateway.createStage(stageParams, (stageError) => {
+                    if (stageError) console.error(stageError, stageError.stack);
+                    else {
+                      const deployParams = {
+                        ApiId: createData.ApiId,
+                        StageName: "relay-dev",
+                      };
+                      this.apiGateway.createDeployment(
+                        deployParams,
+                        (deployError) => {
+                          if (deployError)
+                            console.log(deployError, deployError.stack);
+                          else console.log("Relay API Deployed");
+                        }
+                      );
+                    }
+                  });
+                }
+              });
+            }
           }
         );
       }

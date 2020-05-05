b846ecfd26582541602b766015072b9b4554729c
Gavin Williams <gavinw@theodo.co.uk>
2020-05-05 12:51:07 +0100

    Add wbsockets

--- src/services/relay.js
+++ src/services/relay.js
@@ -1,5 +1,7 @@
 import { Loader } from "../components/loader";
 
+const WebSocket = require("ws");
+
 async function createRelay(apiGateway, fullLambda, program, screen) {
   console.log("Setting up Relay...");
   const loader = new Loader(screen, 5, 20);
@@ -9,7 +11,17 @@ async function createRelay(apiGateway, fullLambda, program, screen) {
       fullLambda,
       program
     );
-    console.log(websocketAddress);
+    const relay = new WebSocket(websocketAddress);
+    relay.on("open", () => {
+      console.log("Warning: Realtime logs will appear faster than CloudWatch");
+    });
+    relay.on("message", (data) => {
+      console.log(data);
+    });
+    relay.on("close", () => {
+      console.log("Relay Closed");
+    });
+    relay.on("error", console.error);
   } catch (e) {
     console.error("Relay Setup Failure");
   }

698fb7c7bc29520864ee7ef63679852dcc7ed5f3
Ben Gowers <beng@theodo.co.uk>
2021-03-03 17:08:34 +0000

    added tests and functionailty for handling multiple option variable in stack name (from serverless config file)

--- __test__/util/serverlessConfigParser.test.js
+++ __test__/util/serverlessConfigParser.test.js
@@ -7,6 +7,7 @@ import {
 } from "../../src/services/severlessConfigParser/helpers";
 import ServerlessConfigParser from "../../src/services/severlessConfigParser/serverlessConfigParser";
 
+const TEST_YAML_FILE_EMPTY = "";
 const TEST_YAML_FILE = `
 service:
   name: test-\${opt:testArg1}
@@ -118,13 +119,28 @@ functions:
 `;
 
 // eslint-disable-next-line no-template-curly-in-string
-const STACK_NAME_WITH_OPT = "test-${opt:testArg1}";
-const STACK_NAME_WITHOUT_OPT = "test-backend";
+const STACK_NAME_OPT = "test-${opt:testArg1}";
+// eslint-disable-next-line no-template-curly-in-string
+const STACK_NAME_MANY_OPTS = "test-${opt:testArg1}-${opt:testArg2}";
+const STACK_NAME_MANY_NO_OPTS = "test-backend-arg2";
+const STACK_NAME_NO_OPT = "test-backend";
 const CMD_VAR_1 = "--testArg1";
 const CMD_VAL_1 = "backend";
 const CMD_VAR_2 = "-testArg2";
 const CMD_VAL_2 = "arg2";
 const INVALID_ARGUMENT_EXIT_CODE = 9;
+
+const setupConfigParser = (yamlFileString) => {
+  fs.readFileSync.mockReturnValue(yamlFileString);
+  fs.existsSync.mockReturnValue(true);
+
+  const program = {
+    args: [CMD_VAR_1, CMD_VAL_1],
+    location: "~/Dev/testProject/backend",
+  };
+
+  return new ServerlessConfigParser(program);
+};
 
 jest.mock("fs");
 
@@ -164,22 +180,29 @@ describe("Serverless Config Options", () => {
    */
   it("should replace the service name option with stored option", () => {
     const testOptions = { testArg1: CMD_VAL_1 };
-    expect(replaceStacknameOpt(STACK_NAME_WITH_OPT, testOptions)).toBe(
-      STACK_NAME_WITHOUT_OPT
+    expect(replaceStacknameOpt(STACK_NAME_OPT, testOptions)).toBe(
+      STACK_NAME_NO_OPT
+    );
+  });
+
+  it("should replace multiple service name option with their respective stored options", () => {
+    const testOptions = { testArg1: CMD_VAL_1, testArg2: CMD_VAL_2 };
+    expect(replaceStacknameOpt(STACK_NAME_MANY_OPTS, testOptions)).toBe(
+      STACK_NAME_MANY_NO_OPTS
     );
   });
 
   it("should not replace the stack name where there is not opt variable in the YAML configuration file", () => {
     const testOptions = { testArg1: CMD_VAL_1 };
-    expect(replaceStacknameOpt(STACK_NAME_WITHOUT_OPT, testOptions)).toBe(
-      STACK_NAME_WITHOUT_OPT
+    expect(replaceStacknameOpt(STACK_NAME_NO_OPT, testOptions)).toBe(
+      STACK_NAME_NO_OPT
     );
   });
 
   it("should output an error message when an opt varibale exists in serverless configuration but no option is passed", () => {
     console.error = jest.fn();
     process.exit = jest.fn();
-    replaceStacknameOpt(STACK_NAME_WITH_OPT, []);
+    replaceStacknameOpt(STACK_NAME_OPT, []);
 
     expect(console.error).toHaveBeenCalledWith(
       CYAN_STRING_FORMAT,
@@ -190,26 +213,21 @@ describe("Serverless Config Options", () => {
 });
 
 describe("Serverless Config Parsing", () => {
-  let SLS;
-
-  beforeAll(() => {
-    fs.readFileSync.mockReturnValue(TEST_YAML_FILE);
-    fs.existsSync.mockReturnValue(true);
-
-    const program = {
-      args: [CMD_VAR_1, CMD_VAL_1],
-      location: "~/Dev/testProject/backend",
-    };
-
-    SLS = new ServerlessConfigParser(program);
-  });
-
   it("should read YAML file and replace opt in service.name with stored arg", () => {
+    const SLS = setupConfigParser(TEST_YAML_FILE);
     expect(SLS.getStackName("dev")).toBe("test-backend-dev");
   });
 
   it("should get a stackname given a stage", () => {
+    const SLS = setupConfigParser(TEST_YAML_FILE);
     const stage = "test";
     expect(SLS.getStackName(stage)).toBe("test-backend-test");
+  });
+
+  it("should continue as normal when YAML file is empty", () => {
+    const SLS = setupConfigParser(TEST_YAML_FILE_EMPTY);
+    expect(SLS.getStage()).toBe("dev");
+    expect(SLS.getStackName("dev")).toBe(null);
+    expect(SLS.getRegion()).toBe(null);
   });
 });
--- src/services/severlessConfigParser/helpers.js
+++ src/services/severlessConfigParser/helpers.js
@@ -1,3 +1,7 @@
+const OPT_REGEX = /\$\{opt:[^{}$]+\}/;
+const OPT_REGEX_GLOBAL = /\$\{opt:[^{}$]+\}/g;
+const REMOVE_BRACKETS_REGEX = /[${}]/g;
+
 export const CYAN_STRING_FORMAT = "\x1b[36m%s\x1b[0m";
 
 export const transformArgsToDict = (args) => {
@@ -12,22 +16,24 @@ export const transformArgsToDict = (args) => {
 };
 
 export const replaceStacknameOpt = (stackname, cmdOptions) => {
-  const optRegex = /\$\{opt:.*\}/;
+  if (!stackname.match(OPT_REGEX)) return stackname;
 
-  if (!stackname.match(optRegex)) return stackname;
+  const variables = stackname.match(OPT_REGEX_GLOBAL);
 
-  let variable = stackname.match(optRegex)[0];
+  variables.forEach((variable) => {
+    variable = variable.replace(REMOVE_BRACKETS_REGEX, "");
+    const varName = variable.split(":")[1];
 
-  variable = variable.replace(/[${}]/g, "");
-  const varName = variable.split(":")[1];
+    if (cmdOptions[varName] === undefined) {
+      console.error(
+        CYAN_STRING_FORMAT,
+        `Your project requires stack name option --${varName} to be passed when starting sls-dev-tools`
+      );
+      process.exit(9);
+    }
 
-  if (cmdOptions[varName] === undefined) {
-    console.error(
-      CYAN_STRING_FORMAT,
-      `Your project requires stack name option --${varName} to be passed when starting sls-dev-tools`
-    );
-    process.exit(9);
-  }
+    stackname = stackname.replace(OPT_REGEX, cmdOptions[varName]);
+  });
 
-  return stackname.replace(/\$\{opt:.*\}/, cmdOptions[varName]);
+  return stackname;
 };

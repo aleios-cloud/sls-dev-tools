3cd58c4fa9c79ee979e420b73df6c47865dd30c2
Ben Gowers <beng@theodo.co.uk>
2021-03-05 17:04:25 +0000

    removed tests

--- src/services/severlessConfig/__test__/constants.js
+++ src/services/severlessConfig/__test__/constants.js
@@ -15,7 +15,25 @@ export const TEST_JSON = {
       name: "aws",
       runtime: "nodejs10.x",
       region: "eu-west-1",
-      stage: "dev",
+    },
+  },
+};
+
+export const TEST_YAML = {
+  test: `
+service: test-backend
+
+provider:
+  name: aws
+  runtime: nodejs10.x
+  region: eu-west-1
+`,
+  result: {
+    service: "test-backend",
+    provider: {
+      name: "aws",
+      runtime: "nodejs10.x",
+      region: "eu-west-1",
     },
   },
 };
